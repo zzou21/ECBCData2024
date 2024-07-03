9adb9a76f38ec07284c3b43b446aa0fcf684beae
Jerry Zou <zhihui.zou@duke.edu>
2024-07-03 11:45:44 -0400

    updated cleaning

--- XMLProcessingAndTraining/ManuscriptMetadata/OperateMetadata.py
+++ XMLProcessingAndTraining/ManuscriptMetadata/OperateMetadata.py
@@ -1,48 +1,52 @@
 import re, os, json
 
-def findPublishingYear(jsonPath):
-    with open(jsonPath, "r") as jsonContent:
-        metadataContent = json.load(jsonContent)
-    
-    counter = 0  #could use this variable to count whichever number the user needs to track.
-    hasDate = 0
-    totalfiles = 0
-    fileNameListBetween = {}
-    
+class metadataCleaning:
+    def __init__(self, mainMetadataJSON):
+        self.mainMetadataJSON = mainMetadataJSON
 
-    for phase, filenameDictionary in metadataContent.items():
-        for fileName, metadataDictionary in filenameDictionary.items():
-            totalfiles += 1
-            if metadataDictionary["DATE"]:
-                hasDate += 1
+    def findPublishingYear(jsonPath):
+        with open(jsonPath, "r") as jsonContent:
+            metadataContent = json.load(jsonContent)
+        
+        counter = 0  #could use this variable to count whichever number the user needs to track.
+        hasDate = 0
+        totalfiles = 0
+        fileNameListBetween = {}
+        
 
-                # print(type(metadataDictionary["DATE"]))
-                # if len(metadataDictionary["DATE"]) > 1:
-                #     print(fileName)
-                # print(metadataDictionary["DATE"][0])
-                numbers = re.findall(r'\b\d{4}\b', metadataDictionary["DATE"][0])
-                if numbers:
+        for phase, filenameDictionary in metadataContent.items():
+            for fileName, metadataDictionary in filenameDictionary.items():
+                totalfiles += 1
+                if metadataDictionary["DATE"]:
+                    hasDate += 1
 
-                    newNum = int(numbers[0])
-                    if newNum >= 1590 and newNum <= 1639:
-                        print("here")
-                        counter += 1
-                        print(fileName)
-                        # fileNameListBetween.append(metadataDictionary[""])
-                        fileNameListBetween[fileName[:6]] = 1
-                        print("added")
+                    # print(type(metadataDictionary["DATE"]))
+                    # if len(metadataDictionary["DATE"]) > 1:
+                    #     print(fileName)
+                    # print(metadataDictionary["DATE"][0])
+                    numbers = re.findall(r'\b\d{4}\b', metadataDictionary["DATE"][0])
+                    if numbers:
+
+                        newNum = int(numbers[0])
+                        if newNum >= 1590 and newNum <= 1639:
+                            print("here")
+                            counter += 1
+                            print(fileName)
+                            # fileNameListBetween.append(metadataDictionary[""])
+                            fileNameListBetween[fileName[:6]] = 1
+                            print("added")
 
 
-    print(f"Counter {counter}")
-    print(f"hasDate {hasDate}")
-    print(f"totalFiles {totalfiles}")
-    print(fileNameListBetween)
-    return fileNameListBetween
+        print(f"Counter {counter}")
+        print(f"hasDate {hasDate}")
+        print(f"totalFiles {totalfiles}")
+        print(fileNameListBetween)
+        return fileNameListBetween
 
 
 if __name__ == "__main__":
     jsonPath = "/Users/Jerry/Desktop/Data+2024/Data+2024Code/ECBCData2024/XMLProcessingAndTraining/ManuscriptMetadata/DocumentMetadata.json"
-    returnDictionary = findPublishingYear(jsonPath)
     outputJSONPath = "/Users/Jerry/Desktop/Data+2024/Data+2024Code/ECBCData2024/XMLProcessingAndTraining/ManuscriptMetadata/FilesBetween15901639.json"
-    with open(outputJSONPath, "w") as file:
-        json.dump(returnDictionary, file, indent=4)
+    # with open(outputJSONPath, "w") as file:
+    #     json.dump(returnDictionary, file, indent=4)
+    metadataCleaningObject = metadataCleaning(jsonPath)
